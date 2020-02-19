276977b9ecfbed25b3ce001cae2e169bdd286d2c
botforge <dhruvkar@seas.upenn.edu>
2020-02-19 13:26:06 -0500

    did accel combination

--- ESE650-Spring2019 Project2/estimate_rot.py
+++ ESE650-Spring2019 Project2/estimate_rot.py
@@ -18,14 +18,14 @@ def load_data(data_num):
 	vicon_raw = io.loadmat(vicon_filename)
 	return imu_raw, vicon_raw
 
-def R_to_rpy(R):
+def R_to_euler(R):
 	"Convert 3x3 R matrix into roll, pitch, yaw"
 	yaw = math.atan(R[1, 0]/R[0, 0])
 	pitch = math.atan(-1.0 * R[2,0] / np.sqrt(R[2,1]**2 + R[2,2]**2))
 	roll = math.atan(R[2, 1]/R[2, 2])
 	return roll, pitch, yaw
 
-def vicon_to_rpy(vicon_raw):
+def vicon_to_euler(vicon_raw):
 	"Get RPY array for each 3x3 R matrix in vicon_raw"
 	rots, ts = vicon_raw.get("rots"), vicon_raw.get("ts")
 	ts = np.squeeze(ts)
@@ -34,14 +34,14 @@ def vicon_to_rpy(vicon_raw):
 	yaws = []
 	for i in range(rots.shape[-1]):
 		R = rots[:, :, i]
-		roll, pitch, yaw = R_to_rpy(R)
+		roll, pitch, yaw = R_to_euler(R)
 		rolls.append(roll)
 		pitches.append(pitch)
 		yaws.append(yaw)
 	return rolls, pitches, yaws
 
 def plot_vicon(vicon_raw):
-	rolls, pitches, yaws = vicon_to_rpy(vicon_raw)
+	rolls, pitches, yaws = vicon_to_euler(vicon_raw)
 	ts = vicon_raw.get("ts")
 	plt.plot(np.squeeze(ts), rolls, label="roll")
 	plt.plot(np.squeeze(ts), pitches, label="pitch")
@@ -50,17 +50,35 @@ def plot_vicon(vicon_raw):
 def process_imu(imu_raw):
 	"""
 	"""
-	zero_g = [46.8230694, 14.56500489, 29.73607038 + 9.8]
+	vals = imu_raw.get("vals")
+	zero_g = np.array([0, 0, -9.8])
 
-	#1) Extract accel and gyro from measurements
-	vals = imu_raw.get("vals").T
-	accel = vals[:, :3]
-	gyro = vals[:, 3:]
+	#1) Extract accel and gyro from measurements & transform
+	#a) Transform Accel according to note@183
+	tf_accel = np.array([
+		[-1, 0, 0],
+		[0, -1, 0],
+		[0, 0, 1]
+	])
+	accel = vals[:3, :]
+	accel = (tf_accel @ accel).T
+
+	#b) Gyro according to note@183
+	tf_gyro = np.array([
+		[0, 0, 1],
+		[1, 0, 0],
+		[0, 1, 0]
+	])
+	gyro = vals[3:, :]
+	gyro = (tf_gyro @ gyro).T
 	
 	#2) Convert ADC -> mV -> delta_mV
-	accel = (accel * 3300) / 1023
-	accel -= zero_g
-	pdb.set_trace()
+	#a) Accelerometer
+	sens_accel = 1
+	scale_accel = 3300/1023/330 #IMU Ref sheet
+	accel = accel * scale_accel
+	bias_accel = np.mean(accel[0:10], axis=0) - zero_g
+	accel = (accel - bias_accel)
 
 	#3) Convert delta_mv into roll, pitch, yaw
 
