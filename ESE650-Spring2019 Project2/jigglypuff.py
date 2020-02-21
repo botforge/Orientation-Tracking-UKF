58f33019fe4b09127abbf33e02dc5bbd473fc12e
botforge <dhruvkar@seas.upenn.edu>
2020-02-20 20:23:10 -0500

    Done process update until mean

--- ESE650-Spring2019 Project2/jigglypuff.py
+++ ESE650-Spring2019 Project2/jigglypuff.py
@@ -1,12 +1,20 @@
 import numpy as np
-from math import sqrt
+from cumquat import *
+import math
 
 class UKF:
     def __init__(self):
         self.x = np.zeros((7,))
         self.P = np.zeros((6, 6))
+        self.Q = np.zeros((6, 6))
         self.k = 0
 
+    def x_omega(self):
+        return self.x[4:]
+    
+    def x_quat(self):
+        return self.x[:4]
+    
     def cholesky(self, B):
         n = B.shape[0]
         L = np.zeros((n, n))
@@ -16,14 +24,65 @@ class UKF:
                 t_sum = sum(L[i, j] * L[k, j] for j in range(k))
 
                 if i == k:
-                    L[i, k] = sqrt(B[i, i] - t_sum)
+                    L[i, k] = math.sqrt(B[i, i] - t_sum)
                 else:
                     L[i, k] = (1.0 / L[k, k] * (B[i, k] - t_sum))
         return L
 
-    def calc_sigma_pts(self, )
+    def orientation_to_quat(self, w, dt=1):
+        """ Convert 3D Orientation to 4D quat """
+        q = np.array([1, 0, 0, 0])
+        if np.linalg.norm(w) != 0:
+            angle = np.linalg.norm(w) * dt
+            axis = w / np.linalg.norm(w)
+            q[0] = math.cos(angle/2)
+            q[1:] = axis * math.sin(angle/2)
+        return q
 
+    def calc_sigma_pts(self):
+        n = self.P.shape[0]
+
+        #Eq 35, 36
+        S = self.cholesky(self.P + self.Q)
+        W_plus = math.sqrt(2*n) * S
+        W_minus = -1.0 * math.sqrt(2*n) * S
+        W = np.hstack(W_plus, W_minus)
     
+        #Create X 
+        q = self.x_quat()
+        omega = self.x_omega()
+        m = W.shape[1]
+        X = np.empty((7, m))
+        for i in range(m):
+            #Eq 34
+            w_q = self.orientation_to_quat(W[:3, i]) #convert 3D to 2D
+            w_omega = W[3:, i] 
+            new_q = quat_mult(q, w_q)
+            new_omega = omega + w_omega
+
+            X[0:4, i] = new_q
+            X[4:, i] = new_omega
+        return X
+
+    def process_update(self, dt):
+        X = self.calc_sigma_pts()
+        m = X.shape[1]
+
+        #1) Construct Y by stepping X forward
+        Y = np.zeros_like(X)
+
+        #a) Update Quaternion portion of each Column in X
+        for i in range(m):
+            q = X[0:4, i]
+            omega = X[4:, i]
+            d_q = self.orientation_to_quat(X[0:4, i], dt=dt)
+
+            Y[0:4, i] = quat_mult(q, d_q)
+            Y[4:, i] = omega
+
+        #2) Recompute Mean and Covariance from Y
+        mean_q = quat_avg(Y[0:4, :])
+        
 
 def unit_test():
     ukf = UKF()
